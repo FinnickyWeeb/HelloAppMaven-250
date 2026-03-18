9dc1a6364a2de0165a12d20520886ff2541fad01
FinnickyWeeb <prathamsharma696@gmail.com>
2026-03-18 12:17:26 +0530

    UC2: Display Hello with Command-Line Argument

--- src/main/java/com/bridgelabz/App.java
+++ src/main/java/com/bridgelabz/App.java
@@ -1,13 +1,10 @@
 package com.bridgelabz;
 
-/**
- * Hello world!
- *
- */
 public class HelloApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
 
 }
